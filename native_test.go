d91dd08b20a5a7b7f605219b448a2c09bd5b5fc4
sashayakovtseva <a.yakovtseva@lucky-team.pro>
2022-05-25 23:50:22 +0300

    Support inlined native requests
    Signed-off-by: sashayakovtseva <a.yakovtseva@lucky-team.pro>

--- native_test.go
+++ native_test.go
@@ -8,16 +8,24 @@ import (
 )
 
 var _ = Describe("Native", func() {
-	var subject *Native
-
-	BeforeEach(func() {
-		Expect(fixture("native", &subject)).To(Succeed())
-	})
-
 	It("should parse correctly", func() {
+		var subject *Native
+		Expect(fixture("native", &subject)).To(Succeed())
 		Expect(subject).To(Equal(&Native{
 			Request: json.RawMessage(`"PAYLOAD"`),
 			Version: "2",
+		}))
+	})
+
+	It("should parse asian correctly", func() {
+		var (
+			subject *Native
+			expect  json.RawMessage
+		)
+		Expect(fixture("native_inline", &subject)).To(Succeed())
+		Expect(fixture("native_inline", &expect)).To(Succeed())
+		Expect(subject).To(Equal(&Native{
+			Request: expect,
 		}))
 	})
 })
