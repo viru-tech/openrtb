3314a45e0445033ee466b0e692b7403948404028
Yuri Dotsya <y.dotsya@lucky-team.pro>
2024-06-25 14:33:41 +0300

    Add eventtrackers response attribute

--- native/response/response_test.go
+++ native/response/response_test.go
@@ -2,45 +2,45 @@ package response
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"os"
+	"reflect"
 	"testing"
-
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Response", func() {
-	It("should parse correctly", func() {
-		Expect(fixture("testdata/response1.json")).To(Equal(&Response{
-			Version:     "1.1",
-			ImpTrackers: []string{"http://imptracker.com"},
-			JSTracker:   "<script>track()</script>",
-			Link: Link{
-				URL: "http://i.am.a/URL",
-			},
-			Assets: []Asset{
-				{ID: 123, Required: 1, Title: &Title{Text: "Learn about this awesome thing"}},
-				{ID: 124, Required: 1, Image: &Image{URL: "http://www.myads.com/thumbnail1.png"}},
-				{ID: 128, Required: 1, Image: &Image{URL: "http://www.myads.com/largethumb1.png"}},
-				{ID: 126, Required: 1, Data: &Data{Value: "My Brand"}},
-				{ID: 127, Required: 1, Data: &Data{Value: "Learn all about this awesome story of someone using my product."}},
-				{ID: 4, Video: &Video{VASTTag: "<VAST version=\"2.0\"></VAST>"}},
-				{ID: 5, Link: &Link{URL: "http://landing.com", ClickTrackers: []string{"http://tracker.com"}, FallbackURL: "http://fallback.com"}},
-			},
-		}))
-	})
-})
-
-func TestSuite(t *testing.T) {
+func TestResponse(t *testing.T) {
 	t.Parallel()
 
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "openrtb/native/response")
+	var subject *Response
+	if err := fixture("testdata/response1.json", &subject); err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	exp := &Response{
+		Version:     "1.1",
+		ImpTrackers: []string{"http://imptracker.com"},
+		JSTracker:   "<script>track()</script>",
+		Link: Link{
+			URL: "http://i.am.a/URL",
+		},
+		Assets: []Asset{
+			{ID: 123, Required: 1, Title: &Title{Text: "Learn about this awesome thing"}},
+			{ID: 124, Required: 1, Image: &Image{URL: "http://www.myads.com/thumbnail1.png"}},
+			{ID: 128, Required: 1, Image: &Image{URL: "http://www.myads.com/largethumb1.png"}},
+			{ID: 126, Required: 1, Data: &Data{Value: "My Brand"}},
+			{ID: 127, Required: 1, Data: &Data{Value: "Learn all about this awesome story of someone using my product."}},
+			{ID: 4, Video: &Video{VASTTag: "<VAST version=\"2.0\"></VAST>"}},
+			{ID: 5, Link: &Link{URL: "http://landing.com", ClickTrackers: []string{"http://tracker.com"}, FallbackURL: "http://fallback.com"}},
+		},
+	}
+	if got := subject; !reflect.DeepEqual(exp, got) {
+		t.Errorf("expected %+v, got %+v", exp, got)
+	}
 }
 
-func fixture(path string) (res *Response) {
-	enc, err := ioutil.ReadFile(path)
-	Expect(err).ToNot(HaveOccurred())
-	Expect(json.Unmarshal(enc, &res)).To(Succeed())
-	return res
+func fixture(path string, v interface{}) error {
+	bin, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bin, v)
 }
